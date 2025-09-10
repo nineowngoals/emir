8ccbe27571868c7870830f9b6516e9938a06547f
nineowngoals <88162117+nineowngoals@users.noreply.github.com>
2025-09-11 00:06:12 +0700

    commit

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// Serve production files at root
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Serve development files at /dev
 app.use('/dev', express.static(path.join(__dirname, 'dev')));
-
-// Optional: redirect /dev to index.html automatically
 app.get('/dev', (req, res) => {
   res.sendFile(path.join(__dirname, 'dev', 'index.html'));
 });
 
+// Serve production files at root
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
